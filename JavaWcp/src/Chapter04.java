3e0814f8fec4973094ef22d55f5c2d0b4611b3a2
sudaaiko <aiko05.hello@gmail.com>
2020-11-09 22:50:23 +0900

    [finish]Chapter_4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "AikoSuda";
+		System.out.println(name);
+
+		name = "須田藍子";
+		System.out.println(name);
+
+		final int LUCKEY_NUM;
+		LUCKEY_NUM = 7;
+		System.out.println(LUCKEY_NUM);
+	}
+
+}
