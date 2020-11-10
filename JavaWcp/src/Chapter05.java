09208c0ad24294f5a1164e42514712fa9da122c7
sudaaiko <aiko05.hello@gmail.com>
2020-11-10 19:33:49 +0900

    [finish]chapter05

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,22 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 12345L;
+		System.out.println(long1);
+		double double1 = 12.244;
+		System.out.println(double1);
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+
+		Float float1 = 0.23f;
+		System.out.println(float1);
+		Integer integer1 = 55;
+		System.out.println(integer1);
+		Character character1 = 'c';
+		System.out.println(character1);
+
+		int[] array1 = new int[] { 10, 20, 30, 40, 50 };
+		System.out.println(array1[3]);
+
+	}
+}
